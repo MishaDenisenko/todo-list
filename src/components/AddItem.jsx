20703540627a42794ea7d6b19deb2c602c057d95
mihailden <midenisenko@gmail.com>
2023-06-30 16:23:54 +0300

    added alert and adding task by enter

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -3,16 +3,33 @@ import React from 'react';
 const AddItem = ({setIncompleteTaskList, incompleteTaskList}) => {
     const handleAddTask = (e) => {
         e.preventDefault();
-        const taskName = document.querySelector('#new-task').value;
+        const addField = document.querySelector('#new-task')
+        const taskName = addField.value.trim();
 
+        if (taskName.length === 0) {
+            alert('Добавьте название задачи для добавления!');
+            return addField.classList.add('empty');
+        }
+
+        addField.value = '';
         setIncompleteTaskList(incompleteTaskList.includes(taskName) ? [...incompleteTaskList]: [taskName, ...incompleteTaskList])
     }
 
+    const handleOnChange = (e) => {
+        e.preventDefault();
+
+        e.target.classList.remove('empty');
+    }
+
+    const handleSubmit = (e) => {
+        if (e.code === 'Enter') handleAddTask(e)
+    }
+
     return (
-        <div>
-            <label htmlFor="new-task">Add Item</label>
-            <input id="new-task" type="text"/>
-            <button onClick={handleAddTask}>Add</button>
+        <div className={'add-block'}>
+            <label className='add-label' htmlFor="new-task">Add Item</label>
+            <input className='add-field' onChange={handleOnChange} onKeyDown={handleSubmit} id="new-task" type="text"/>
+            <button  onClick={handleAddTask}>Add</button>
         </div>
     );
 };
